e4875af26dce4db2d70ab96cc5c981d521d05253
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-05-01 17:26:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,20 @@
+require("dotenv").config(); // 👈 MUST
+
 const mongoose = require("mongoose");
 
 const mongoUri = process.env.MONGODB_URI;
 
 if (!mongoUri) {
-  throw new Error("MONGODB_URI is not set");
+  console.error("❌ MONGODB_URI not found. Create Backend/.env from .env.example");
+  process.exit(1);
 }
 
-mongoose.connect(mongoUri)
+mongoose
+  .connect(mongoUri, { serverSelectionTimeoutMS: 5000 })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("DB Error:", err));
+  .catch((err) => {
+    console.error("DB Error:", err?.message || err);
+    process.exit(1);
+  });
 
 module.exports = mongoose;
