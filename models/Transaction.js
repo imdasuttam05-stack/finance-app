345d6a12daea20dbc9e3547c5c69a6dd68c20917
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-05-01 16:52:44 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -4,16 +4,46 @@ const transactionSchema = new mongoose.Schema({
   type: {
     type: String,
     enum: ["income", "expense", "investment", "loan"],
+    required: true,
   },
+
   subType: {
     type: String, // asset / liability (for loan)
+    enum: ["asset", "liability", ""],
+    default: "",
   },
-  amount: Number,
-  note: String,
+
+  category: {
+    type: String,
+    default: "",
+  },
+
+  subCategory: {
+    type: String,
+    default: "",
+  },
+
+  // 👉 NEW: person (ledger system)
+  personId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Person",
+    default: null,
+  },
+
+  amount: {
+    type: Number,
+    required: true,
+  },
+
+  note: {
+    type: String,
+    default: "",
+  },
+
   date: {
     type: Date,
     default: Date.now,
   },
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Transaction", transactionSchema);
